2915918e6f19be06e8e30e0cc94fa98f581d072e
Antonio <AntonioKL@users.noreply.github.com>
2019-01-19 21:17:01 +0200

    Update UpdateSubscriberSubscriptionCommandHandler.php

--- app/Bus/Handlers/Commands/Subscriber/UpdateSubscriberSubscriptionCommandHandler.php
+++ app/Bus/Handlers/Commands/Subscriber/UpdateSubscriberSubscriptionCommandHandler.php
@@ -11,53 +11,32 @@
 
 namespace CachetHQ\Cachet\Bus\Handlers\Commands\Subscriber;
 
-use CachetHQ\Cachet\Bus\Commands\Subscriber\UpdateSubscriberSubscriptionCommand;
-use CachetHQ\Cachet\Bus\Events\Subscriber\SubscriberHasUpdatedSubscriptionsEvent;
+use CachetHQ\Cachet\Bus\Commands\Subscriber\UpdateSubscriberSlackSubscriptionCommand;
 use CachetHQ\Cachet\Models\Component;
 use CachetHQ\Cachet\Models\Subscriber;
-use CachetHQ\Cachet\Models\Subscription;
 
 /**
  * This is the subscribe subscriber command handler.
  *
  * @author Joseph Cohen <joe@alt-three.com>
  */
-class UpdateSubscriberSubscriptionCommandHandler
+class UpdateSubscriberSlackSubscriptionCommandHandler
 {
     /**
      * Handle the subscribe subscriber command.
      *
-     * @param \CachetHQ\Cachet\Bus\Commands\Subscriber\UpdateSubscriberSubscriptionCommand $command
+     * @param \CachetHQ\Cachet\Bus\Commands\Subscriber\UpdateSubscriberSlackSubscriptionCommand $command
      *
      * @return \CachetHQ\Cachet\Models\Subscriber
      */
-    public function handle(UpdateSubscriberSubscriptionCommand $command)
+    public function handle(UpdateSubscriberSlackSubscriptionCommand $command)
     {
         $subscriber = $command->subscriber;
-        $subscriptions = $command->subscriptions ?: [];
+        $slack_url = $command->slack_url;
 
-        $components = Component::all();
-
-        $updateSubscriptions = $components->filter(function ($item) use ($subscriptions) {
-            return in_array($item->id, $subscriptions);
-        });
-
-        $subscriber->global = ($updateSubscriptions->count() === $components->count());
-
-        $subscriber->subscriptions()->delete();
-
-        if (!$updateSubscriptions->isEmpty()) {
-            $updateSubscriptions->each(function ($subscription) use ($subscriber) {
-                Subscription::firstOrCreate([
-                    'subscriber_id' => $subscriber->id,
-                    'component_id'  => $subscription->id,
-                ]);
-            });
-        }
+        $subscriber->slack_webhook_url = $slack_url;
 
         $subscriber->save();
-
-        event(new SubscriberHasUpdatedSubscriptionsEvent($subscriber));
 
         return $subscriber;
     }
